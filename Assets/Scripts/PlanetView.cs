544652efaa0f2cbdad49ee0ab9c783e533149e0f
John Smith <42hackll@gmail.com>
2021-02-05 18:31:37 +0300

    add simple planet rotation

---
+++ Assets/Scripts/PlanetView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlanetView : MonoBehaviour
+{
+    public GameObject Planet;
+    public float SpeedRotation = 2f;
+    
+    public void FixedUpdate()
+    {
+        Planet.transform.Rotate(0,0,SpeedRotation);
+    }
+}
